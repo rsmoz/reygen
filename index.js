dbc1c8a765038c6d6a07368068b739e880f6db71
Robert Mozayeni <dev@mzy.me>
2015-12-30 06:07:08 -0500

    download json

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var http = require('http');
+var fs = require('fs');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -13,7 +15,23 @@ app.get('/', function(request, response) {
 	response.status(200).send('hello world');
 });
 
+
+//hackmyresume does not define a JS API, so I'm calling it from bash. Bleh.
+app.get('/generate', function (request, response) {
+	var file = fs.createWriteStream("target.json");
+	var source = req.param('json');
+	var dl = http.get('source', function(response) {
+		response.pipe(file);
+	});
+	
+});
+
 app.listen(app.get('port'), function() {
 	console.log('Node app is running on port', app.get('port'));
 });
+
+
+
+
+
 
